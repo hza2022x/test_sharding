880bad0aa230cd8a589be8255446c00301ccf731
wangxiaoming <wangxiaoming@joyy.com>
2022-10-26 16:16:31 +0800

    init

---
+++ scripts/instance1.sql
@@ -0,0 +1,18 @@
+
+CREATE DATABASE IF NOT EXISTS `test_four_0` DEFAULT CHARACTER SET utf8mb4 ;
+
+CREATE DATABASE IF NOT EXISTS `test_four_1` DEFAULT CHARACTER SET utf8mb4 ;
+
+USE `test_four_0`;
+create table `test_0`(id int primary key,name varchar(10)) ;
+create table `test_1`(id int primary key,name varchar(10)) ;
+create table `test_2`(id int primary key,name varchar(10)) ;
+create table `test_3`(id int primary key,name varchar(10)) ;
+
+USE `test_four_1`;
+create table `test_0`(id int primary key,name varchar(10)) ;
+create table `test_1`(id int primary key,name varchar(10)) ;
+create table `test_2`(id int primary key,name varchar(10)) ;
+create table `test_3`(id int primary key,name varchar(10)) ;
+
+
---
+++ scripts/instance2.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS `test_four_2` DEFAULT CHARACTER SET utf8mb4 ;
+
+CREATE DATABASE IF NOT EXISTS `test_four_3` DEFAULT CHARACTER SET utf8mb4 ;
+
+USE `test_four_2`;
+create table `test_0`(id int primary key,name varchar(10)) ;
+create table `test_1`(id int primary key,name varchar(10)) ;
+create table `test_2`(id int primary key,name varchar(10)) ;
+create table `test_3`(id int primary key,name varchar(10)) ;
+
+USE `test_four_3`;
+create table `test_0`(id int primary key,name varchar(10)) ;
+create table `test_1`(id int primary key,name varchar(10)) ;
+create table `test_2`(id int primary key,name varchar(10)) ;
+create table `test_3`(id int primary key,name varchar(10)) ;
